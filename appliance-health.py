53448b0dd6d2e11ca1ff05644d87cc49e82f5336
Uthman Al-Ayek Eqbal <135247149+uthm4n@users.noreply.github.com>
2023-06-21 08:34:56 +0100

    Implement configurable polling interval
    https://github.com/uthm4n/polling-health-api/issues/1

--- appliance-health.py
+++ appliance-health.py
@@ -3,7 +3,11 @@ import time
 import logging 
 import sys
 import json
- 
+
+polling_interval = input('How frequently (in seconds) would you like the API call to run?')
+# if polling_interval = None:      # test implementation of a default polling interval 
+#   polling_interval = 5
+ #  break 
 try:
     while True:
         url = "https://{YOUR-MORPHEUS-APPLIANCE-URL}/api/health"        # replace me with your Morpheus appliance URL (https://docs.morpheusdata.com/en/latest/getting_started/appliance_setup/appliance_setup.html)
@@ -14,7 +18,7 @@ try:
         response = requests.get(url, headers=headers, verify=False)
         string = response.json()
         print(json.dumps(string, indent=2))
-        time.sleep(5)                                                   # poll every 5 seconds 
+        time.sleep({polling_interval})                                                   # poll every X seconds based on user input on line 7
 except KeyboardInterrupt:                                               # runs indefinitely until a KeyboardInterrupt is triggered 
   logging.info("exiting...")
   sys.exit()
